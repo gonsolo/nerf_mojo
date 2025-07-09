faa7cb7463e35e98a2779b1d2cc09313cd8bc4fd
Gon Solo <gonsolo@gmail.com>
2025-07-09 17:38:20 +0200

    Extract PositionalEncoder.

--- train.py
+++ train.py
@@ -12,7 +12,7 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import axes3d
 from tqdm import trange
 import sys
-from nerf import NeRF
+from nerf import NeRF, PositionalEncoder
 
 # For repeatability
 # seed = 3407
@@ -175,43 +175,6 @@ plt.title('Stratified Sampling (blue) with Perturbation (red)')
 ax = plt.gca()
 ax.axes.yaxis.set_visible(False)
 plt.grid(True)
-
-class PositionalEncoder(nn.Module):
-  r"""
-  Sine-cosine positional encoder for input points.
-  """
-  def __init__(
-    self,
-    d_input: int,
-    n_freqs: int,
-    log_space: bool = False
-  ):
-    super().__init__()
-    self.d_input = d_input
-    self.n_freqs = n_freqs
-    self.log_space = log_space
-    self.d_output = d_input * (1 + 2 * self.n_freqs)
-    self.embed_fns = [lambda x: x]
-
-    # Define frequencies in either linear or log scale
-    if self.log_space:
-      freq_bands = 2.**torch.linspace(0., self.n_freqs - 1, self.n_freqs)
-    else:
-      freq_bands = torch.linspace(2.**0., 2.**(self.n_freqs - 1), self.n_freqs)
-
-    # Alternate sin and cos
-    for freq in freq_bands:
-      self.embed_fns.append(lambda x, freq=freq: torch.sin(x * freq))
-      self.embed_fns.append(lambda x, freq=freq: torch.cos(x * freq))
-
-  def forward(
-    self,
-    x
-  ) -> torch.Tensor:
-    r"""
-    Apply positional encoding to input.
-    """
-    return torch.concat([fn(x) for fn in self.embed_fns], dim=-1)
 
 # Create encoders for points and view directions
 encoder = PositionalEncoder(3, 10)
